20b8c1532e6b0b8aad348393d11e39e9e21bf3e5
Lera Burtseva <burtsevaleria@gmail.com>
2022-09-28 13:50:01 +0300

    Initial commit

---
+++ src/main/java/lerabu/project/NayurruBot/NayurruBotApplication.java
@@ -0,0 +1,13 @@
+package lerabu.project.NayurruBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NayurruBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NayurruBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lerabu/project/NayurruBot/NayurruBotApplicationTests.java
@@ -0,0 +1,13 @@
+package lerabu.project.NayurruBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NayurruBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
